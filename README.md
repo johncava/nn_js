17860190fbaebba2d57eb4aaaa9c0adfacb93719
johncava <jcava@asu.edu>
2017-04-22 23:22:14 -0700

    added some comments and documentation

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # nn_js
+
+Neural Network Implementation in Javascript
+
+# Notes
+
+numeric.dotMM(x,y) - Does matrix multiplication with matrix "x" and "y"
+
+numeric.mul(x,y) - Does scalar multiplication with "x" and "y" regardless if x and/or y are either a scalar (single number) or matrix
+
+numeric.div(x,y) and numeric.add(x,y) used in this case for doing scalar addition and division with a matrix
+
+
--- nn.js
+++ nn.js
@@ -1,10 +1,16 @@
+//Use numericjs library
 var numeric = require('numeric')
 
+//Input Matrix is 4 x 3 (rows x cols)
 var input = [  [0,0,1],
                 [0,1,1],
                 [1,0,1],
                 [1,1,1]]
+
+//Supervised Data Matrix 4 x 1 
+//DO NOT USE [0,0,1,1] *Would be considered as a vector than a matrix*
 var y = [[0],[0],[1],[1]]
+//Randomize the "weight" matrices that connect the hidden layers with both output and input layers
 var weights1 = numeric.random([3,5])
 var weights2 = numeric.random([5,4])
 var weights3 = numeric.random([4,1])
@@ -12,14 +18,20 @@ var weights3 = numeric.random([4,1])
 var hiddenLayer1
 var hiddenLayer2
 
+//Sigmoid Function
+//  return 1/(1 + e^(-x))
 function sigmoid(x){
     return numeric.div(1,(numeric.add(1,numeric.exp(numeric.neg(x)))))
 }
 
+//Derivative of the sigmoid function
+//  return x * (1 - x)
 function sigmoid_derivative(x){
     return numeric.mul(x, numeric.sub(1,x)) 
 }
 
+//Feedforward function
+//  Matrix multiply the input with the weight matrices in a "forward" fashion
 function feedforward(){
     hiddenLayer1 = sigmoid(numeric.dotMMbig(input,weights1));
     hiddenLayer2 = sigmoid(numeric.dotMMbig(hiddenLayer1,weights2));
@@ -55,5 +67,6 @@ function neural_network(){
     }
 }
 
+//Start the neural network
 neural_network();
 
